1b1ff894c616253c8ae01402b77237737ab21733
southglory <coolbirds@naver.com>
2023-01-02 20:33:34 +0900

    [#6] Collision, Raycast

---
+++ Scripts/TestCollision.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    // 1) 나 혹은 상대한테 RigidBody 있어야 한다. (IsKinematic : Off)
+    // 2) 나한테 Collider가 있어야 한다. (isTrigger : Off)
+    // 3) 상대한테 Collider가 있어야 한다. (isTrigger : Off)
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Collision @ {collision.gameObject.name} !");
+    }
+
+
+    // 1) 둘 다 Collider가 있어야 한다.
+    // 2) 둘 중 하나는 IsTrigger : On
+    // 3) 둘 중 하나는 RigidBody가 있어야 한다.
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Debug.Log($"Trigger @ {other.gameObject.name} !");
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, Vector3.forward, 10);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Debug.Log($"Racast {hit.collider.gameObject.name}!");
+        }
+    }
+}
