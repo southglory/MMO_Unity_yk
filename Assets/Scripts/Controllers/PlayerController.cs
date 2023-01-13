b8fbf5eb375d7a5fb0f77ab070853c7045d1cab4
YoungKwang Nam <coolbirds@naver.com>
2023-01-13 16:03:30 +0900

    [#17] Scene manager

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -16,9 +16,6 @@ public class PlayerController : MonoBehaviour
 		//Managers.Input.KeyAction += OnKeyboard;
 		Managers.Input.MouseAction -= OnMouseClicked;
 		Managers.Input.MouseAction += OnMouseClicked;
-
-		// TEMP
-		Managers.UI.ShowSceneUI<UI_Inven>();
     }
 
 
